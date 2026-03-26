e66e58012c8d41405d0a669087ea481c14808fbb
noah <ntholbro@uvm.edu>
2026-03-26 15:22:27 -0700

    new file with a scene explaining why we don't use linear regression

---
+++ linear_to_logistic.py
@@ -0,0 +1,70 @@
+from manim import *
+from MF_Tools import *
+# linear model
+# explain how to interpret the coefficients
+# Instead we need f of y is the linear combination
+# Or it's f inverse
+# The function that we use here is the logit
+class LinearLogisticScene(Scene):
+    def construct(self):
+        tex1 = MathTex(r"P(Y=1) = \beta_0+\beta_1 X_1+\beta_2 X_2+\ldots+\beta_{k-1} X_{k-1}")
+        tex2 = MathTex(r"p = \beta_0+\beta_1 X_1+\beta_2 X_2+\ldots+\beta_{k-1} X_{k-1}")
+        tex3 = MathTex(r"f(p) = \beta_0+\beta_1 X_1+\beta_2 X_2+\ldots+\beta_{k-1} X_{k-1}")
+        tex4 = MathTex(r"p = f^{-1}(\beta_0+\beta_1 X_1+\beta_2 X_2+\ldots+\beta_{k-1} X_{k-1})")
+        tex5 = MathTex(r"\ln\frac{p}{1-p} = \beta_0+\beta_1 X_1+\beta_2 X_2+\ldots+\beta_{k-1} X_{k-1}")
+
+        self.play(Write(tex1))
+        self.play(TransformByGlyphMap(tex1, tex2,
+                                    ([0,1,2,3,4,5], [0])))
+        
+        # Graph the thing
+        self.play(FadeOut(tex2))
+
+        axes = Axes(x_range = [-5,5,1], 
+                    y_range = [-3,3,1],
+                    ).add_coordinates()
+        axis_labels = axes.get_axis_labels(x_label = "X",
+                                           y_label = "p")
+        func = axes.plot((lambda x : 0.38 * (x  - 0.52)), 
+                         color = GREEN)
+        graph_group = VGroup(axes, axis_labels, func)
+        self.play(DrawBorderThenFill(axes), Write(axis_labels), run_time = 0.5)
+        self.play(Create(func))
+
+        hlines = {}
+        areas = {}
+        for y in 0,1:
+            hlines[y] = axes.plot((lambda x : y), color = WHITE)
+            self.add(hlines[y])
+            graph_group.add(hlines[y])
+            areas[y] = axes.get_area(
+                hlines[y],
+                x_range=[-5, 5],
+                bounded_graph=axes.plot(lambda x: (y - 0.5) * 6),  # goes up to y=3 for 1 and down to -3 for 0
+                color=RED,
+                opacity=0.2
+            )
+            self.add(areas[y])
+            graph_group.add(areas[y])
+
+        self.play(FadeOut(graph_group))
+
+        self.play(FadeIn(tex2))
+        self.play(TransformByGlyphMap(tex2, tex3,
+                                      (FadeIn, [0,1,3])))
+        tex3_original = tex3.copy()
+        self.play(TransformByGlyphMap(tex3, tex4,
+                                      ([0],[2], {"path_arc":-PI}),
+                                      ([1],[5], {"path_arc":-PI}),
+                                      ([3],[31],{"path_arc":-PI/6}),
+                                      (FadeIn, [3,4])))
+        #Invert that transform because we want tex3
+        self.play(TransformByGlyphMap(tex4, tex3_original,
+            ([2],[0], {"path_arc": PI}),
+            ([5],[1], {"path_arc": PI}),
+            ([31],[3], {"path_arc": PI/6}),
+            ([3,4], FadeOut)
+        ))
+
+        self.play(TransformByGlyphMap(tex3_original, tex5,
+                                      (range(4), range(7))))
