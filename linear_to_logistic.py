386976a6a8cbb5f9fdbf4dee4d3b3b879f65db72
noah <ntholbro@uvm.edu>
2026-04-16 22:17:05 -0700

    linear_to_logistic has new stuff that's more cohesive with the other scenes

--- linear_to_logistic.py
+++ linear_to_logistic.py
@@ -1,11 +1,31 @@
-import math
 from manim import *
 from MF_Tools import *
 from manim_voiceover import VoiceoverScene
 from manim_voiceover.services.stitcher import _StitcherService as StitcherService
-
-from manim import *
 import numpy as np
+import pandas as pd
+
+df = pd.read_csv("breast_cancer_sklearn.csv")
+X = np.array(df[df.columns[0]])
+Y = np.array(df["target"])
+
+def simple_linear_regression(X, Y):
+    X = np.asarray(X)
+    Y = np.asarray(Y)
+    
+    if X.shape != Y.shape:
+        raise ValueError("X and Y must have the same shape")
+    
+    x_mean = X.mean()
+    y_mean = Y.mean()
+    
+    # slope (beta_1)
+    beta_1 = np.sum((X - x_mean) * (Y - y_mean)) / np.sum((X - x_mean)**2)
+    
+    # intercept (beta_0)
+    beta_0 = y_mean - beta_1 * x_mean
+    
+    return beta_0, beta_1
 
 def create_graph(
     func,
@@ -85,51 +105,43 @@ class LinearLogisticScene(VoiceoverScene):
                 cache_dir="/Users/noah/Convex/LikelihoodShape/cache_dir",
                 min_silence_len=2000,
                 keep_silence=(0,0)))
+        
+        with self.voiceover("a") as tracker: # TODO: We added this voiceover
+            x_range = max(X) - min(X)
+            axes = Axes(x_range = [min(X) - 0.1 * x_range, max(X) + 0.1 * x_range], y_range = [-0.2, 1.2])
+            axis_labels = axes.get_axis_labels(x_label = "X1", y_label = "Y")
+            dots = VGroup(*[Dot(axes.c2p(x, y), color = DARK_BLUE) for x, y in zip(X, Y)])
+            
+            self.play(DrawBorderThenFill(axes))
+            self.play(Write(axis_labels))
+            self.play(LaggedStart(*[Write(dot) for dot in dots], lag_ratio=0.005))
+
+            # Regression coefficients
+            beta0, beta1 = simple_linear_regression(X, Y)
+            regression_line = axes.plot(lambda x : beta0 + beta1 * x,
+                                        x_range = [(1.2 - beta0) / beta1, (-0.2 - beta0) / beta1],
+                                        color = RED)
+            self.play(Create(regression_line))
+            scatterplot = VGroup(axes, axis_labels, dots, regression_line)
+            self.play(FadeOut(scatterplot))
+
+        tex0 = MathTex(r"P(Y=1) = \beta_0+\beta_1 X")
         tex1 = MathTex(r"P(Y=1) = \beta_0+\beta_1 X_1+\beta_2 X_2+\ldots+\beta_{k-1} X_{k-1}")
         tex2 = MathTex(r"p = \beta_0+\beta_1 X_1+\beta_2 X_2+\ldots+\beta_{k-1} X_{k-1}")
         tex3 = MathTex(r"f(p) = \beta_0+\beta_1 X_1+\beta_2 X_2+\ldots+\beta_{k-1} X_{k-1}")
         tex4 = MathTex(r"p = f^{-1}(\beta_0+\beta_1 X_1+\beta_2 X_2+\ldots+\beta_{k-1} X_{k-1})")
         tex5 = MathTex(r"\ln\frac{p}{1-p} = \beta_0+\beta_1 X_1+\beta_2 X_2+\ldots+\beta_{k-1} X_{k-1}")
 
+        with self.voiceover("a") as tracker: # TODO: We added this voiceover
+            self.play(Write(tex0))
+
         with self.voiceover("a") as tracker:
-            self.play(Write(tex1))
+            self.play(TransformMatchingTex(tex0, tex1))
         with self.voiceover("a") as tracker:
             self.play(TransformByGlyphMap(tex1, tex2,
                                         ([0,1,2,3,4,5], [0])))
         
-        # Graph the thing
-        graph_group = create_graph((lambda x : 0.38 * (x  - 0.52)),
-                                   x_range = [-5,5,1],
-                                   y_range = [-3,3,1],
-                                   x_label = "X",
-                                   y_label = "p",
-                                   color = GREEN)
-        axes = graph_group[0]
-        with self.voiceover("a") as tracker:
-            self.play(FadeOut(tex2))
-            self.add(graph_group[0],graph_group[1])
-            self.play(Create(graph_group[2]))
-
-        with self.voiceover("a") as tracker:
-            hlines = {}
-            areas = {}
-            for y in 0,1:
-                hlines[y] = axes.plot((lambda x : y), color = WHITE)
-                self.add(hlines[y])
-                graph_group.add(hlines[y])
-                areas[y] = axes.get_area(
-                    hlines[y],
-                    x_range=[-5, 5],
-                    bounded_graph=axes.plot(lambda x: (y - 0.5) * 6),  # goes up to y=3 for 1 and down to -3 for 0
-                    color=RED,
-                    opacity=0.2
-                )
-                self.add(areas[y])
-                graph_group.add(areas[y])
-
-        with self.voiceover("a") as tracker:
-            self.play(FadeOut(graph_group))
-            self.play(FadeIn(tex2))
+        # TODO: There are 2 voiceovers in here that we removed
         with self.voiceover("a") as tracker:
             self.play(TransformByGlyphMap(tex2, tex3,
                                         (FadeIn, [0,1,3])))
