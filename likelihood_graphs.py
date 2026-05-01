54f2cdc6c8a802cd5e31f8fff087c70a2eef716b
noah <ntholbro@uvm.edu>
2026-04-30 21:10:26 -0700

    put the likelihood graph making in a function

--- likelihood_graphs.py
+++ likelihood_graphs.py
@@ -121,54 +121,60 @@ def surface_from_function(
 
     return surface
 
-x_radius = se0 * 0.65
-y_radius = se1 * 0.65
-beta0, beta1 = beta.reshape(-1)
-x_range = (beta0 - x_radius, beta0 + x_radius)
-y_range = (beta1 - y_radius, beta1 + y_radius)
-print(f"{x_range=}{y_range=}")
+def create_mle_graph(x_radius,
+                     y_radius,
+                     resolution = 32):
+    beta0, beta1 = beta.reshape(-1)
+    x_range = (beta0 - x_radius, beta0 + x_radius)
+    y_range = (beta1 - y_radius, beta1 + y_radius)
+    print(f"{x_range=}{y_range=}")
 
-loglik_range = compute_z_range(
-    z_func=loglik,
-    x_range=x_range,
-    y_range=y_range,
-    samples=30,
-    padding = 0
-)[0:2]
-loglik_offset = np.mean(np.array(loglik_range))
+    loglik_range = compute_z_range(
+        z_func=loglik,
+        x_range=x_range,
+        y_range=y_range,
+        samples=30,
+        padding = 0
+    )[0:2]
+    loglik_offset = np.mean(np.array(loglik_range))
 
-def lik_scaled(beta_hat0, beta_hat1):
-    return np.exp(loglik(beta_hat0, beta_hat1) - loglik_offset)
+    def lik_scaled(beta_hat0, beta_hat1):
+        return np.exp(loglik(beta_hat0, beta_hat1) - loglik_offset)
 
-mle_lik_scaled = np.exp(mle_loglik - loglik_offset)
+    mle_lik_scaled = np.exp(mle_loglik - loglik_offset)
 
-z_range = compute_z_range(
-    z_func=lik_scaled,
-    x_range=x_range,
-    y_range=y_range,
-    samples=30
-)
+    z_range = compute_z_range(
+        z_func=lik_scaled,
+        x_range=x_range,
+        y_range=y_range,
+        samples=30
+    )
 
+    print(f"{x_range=} {y_range=} {z_range=}")
 
-print(f"{x_range=} {y_range=} {z_range=}")
+    axes = ThreeDAxes(
+        x_range=(*x_range, (x_range[1] - x_range[0]) / 4),
+        y_range=(*y_range, (y_range[1] - y_range[0]) / 4),
+        z_range=z_range,
+    )
+
+    surface = surface_from_function(
+        z_func=lik_scaled,
+        axes=axes,
+        x_range=x_range,
+        y_range=y_range,
+        resolution=resolution,
+        color=BLUE_C,
+    )
+
+    return axes, surface
 
 
 class PlotSurfaceExample(ThreeDScene):
     def construct(self):
-        axes = ThreeDAxes(
-            x_range=(*x_range, (x_range[1] - x_range[0]) / 4),
-            y_range=(*y_range, (y_range[1] - y_range[0]) / 4),
-            z_range=z_range,
-        )
-
-        surface = surface_from_function(
-            z_func=lik_scaled,
-            axes=axes,
-            x_range=x_range,
-            y_range=y_range,
-            resolution=39,
-            color=BLUE_C,
-        )
+        axes, surface = create_mle_graph(x_radius = se0 * 0.62,
+                                         y_radius = se1 * 0.62,
+                                         resolution=21)
 
         if True:
             self.set_camera_orientation(
@@ -179,8 +185,8 @@ class PlotSurfaceExample(ThreeDScene):
 
         self.add(axes)
         self.play(Create(surface))
-        mle_dot_scaled = Dot3D(axes.c2p(beta0, beta1, mle_lik_scaled), color = RED)
-        self.play(FadeIn(mle_dot_scaled))
+        # mle_dot_scaled = Dot3D(axes.c2p(beta0, beta1, mle_lik_scaled), color = RED)
+        # self.play(FadeIn(mle_dot_scaled))
         self.wait(1)
         self.begin_ambient_camera_rotation(rate=0.2)
         self.wait(6)
