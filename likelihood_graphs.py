a7c94eecaf9ea57aef0fc6e27839bb50ccefcd9f
noah <ntholbro@uvm.edu>
2026-05-02 23:37:11 -0700

    allow this to pass in loglik_generator

--- likelihood_graphs.py
+++ likelihood_graphs.py
@@ -61,7 +61,6 @@ def create_3d_graph(z_func,
     )
 
     return axes, surface
-    
 
 def create_likelihood_graph(X,
                             y,
@@ -69,15 +68,13 @@ def create_likelihood_graph(X,
                             y_ses = 1,
                             add_intercept = True,
                             resolution = 32,
-                            color = BLUE_C):
+                            color = BLUE_C,
+                            use_loglik = False):
     beta, cov, se = logistic_regression(X, y, add_intercept = add_intercept, return_stats = True)
-    beta0, beta1 = beta.flatten()
-    se0, se1 = se.flatten()
-
-    x_range = (beta0 - se0 * x_ses, beta0 + se0 * x_ses)
-    y_range = (beta1 - se1 * y_ses, beta1 + se1 * y_ses)
-
-    return create_3d_graph(z_func = lik_scaled_generator(X, y),
+    x_range = (beta[0] - se[0] * x_ses, beta[0] + se[0] * x_ses)
+    y_range = (beta[1] - se[1] * y_ses, beta[1] + se[1] * y_ses)
+    z_func = loglik_generator(X, y) if use_loglik else lik_scaled_generator(X, y)
+    return create_3d_graph(z_func = z_func,
                            x_range=x_range,
                            y_range = y_range,
                            z_range = "auto",
