722b2f6e115d7382bedee3e125355c2e6fcce3a2
noah <ntholbro@uvm.edu>
2026-05-05 09:35:06 -0700

    paste in the code from likelihood_graph for more control

--- the_flaw.py
+++ the_flaw.py
@@ -1,16 +1,24 @@
 import numpy as np
 import data # type: ignore
 from manim import *
-from N_Tools import create_likelihood_graph
+from N_Tools import *
 
 class FlawScene(ThreeDScene):
     def construct(self):
-        axes, surface = create_likelihood_graph(data.X[:,0], 
-                                            data.y,
-                                            x_ses = 0.01,
-                                            y_ses = 0.01,
-                                            use_loglik=True,
-                                            resolution=21)
+        X = as_col(data.X[:,0])
+        y = data.y
+        ses = 0.03
+
+        beta, cov, se = logistic_regression(X, y, add_intercept = True, return_stats = True)
+        x_range = (beta[0] - se[0] * ses, beta[0] + se[0] * ses)
+        y_range = (beta[1] - se[1] * ses, beta[1] + se[1] * ses)
+        z_func = loglik_generator(X, y, add_intercept=True)
+        axes, surface = create_3d_graph(z_func = z_func,
+                                        x_range=x_range,
+                                        y_range = y_range,
+                                        z_range = "auto",
+                                        resolution=21,
+                                        color = BLUE_C)
         
         self.set_camera_orientation(
             phi=55 * DEGREES,
