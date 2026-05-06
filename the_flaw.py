c976473d969f1b048bdb6e37d41948a6ed9366ec
noah <ntholbro@uvm.edu>
2026-05-05 23:03:31 -0700

    comment labels for sections

--- the_flaw.py
+++ the_flaw.py
@@ -5,10 +5,10 @@ from N_Tools import *
 
 class FlawScene(ThreeDScene):
     def construct(self):
+        # --- Get our actual log likelihood ---
         X = as_col(data.X[:,0])
         y = data.y
         ses = 0.03
-
         beta, cov, se = logistic_regression(X, y, add_intercept = True, return_stats = True)
         x_range = (beta[0] - se[0] * ses, beta[0] + se[0] * ses)
         y_range = (beta[1] - se[1] * ses, beta[1] + se[1] * ses)
@@ -26,6 +26,7 @@ class FlawScene(ThreeDScene):
                                         resolution=21,
                                         color = BLUE_C)
         
+        # --- Make the surface ---
         self.set_camera_orientation(
             phi=55 * DEGREES,
             theta=-45 * DEGREES,
@@ -35,12 +36,14 @@ class FlawScene(ThreeDScene):
         self.play(Create(surface))
         self.wait(1)
         self.begin_ambient_camera_rotation(rate=0.2)
+
+        # --- Add the MLE dot ---
         mle_x, mle_y = beta
         mle_z = log_likelihood(X, y, beta, add_intercept = True)
-        
         mle_dot = Dot3D(axes.c2p(mle_x, mle_y, mle_z), color = RED)
         self.play(FadeIn(mle_dot))
 
+        # --- Define the functions for what if the log likelihood is something else ---
         def upside_down_loglik(beta_hat0, beta_hat1):
             return 2 * mle_z - loglik(beta_hat0, beta_hat1)
         def saddle_loglik(beta_hat0, beta_hat1):
@@ -55,14 +58,11 @@ class FlawScene(ThreeDScene):
             z_scale = (z_range[1] - z_range[0]) / 4
             exponential = np.exp(-(x_screen**2 + y_screen**2))
             return exponential * z_scale
-        
         def bumped_loglik(beta_hat0, beta_hat1):
             return loglik(beta_hat0, beta_hat1) + bump(beta_hat0, beta_hat1)
-        # --- What if it's a local minimum ---
-
+        
+        # --- Play the animations for what if it's a minimum, saddle, or local non global max ---
         for z_func in upside_down_loglik, saddle_loglik, bumped_loglik:
-
-
             _, surface2 = create_3d_graph(z_func = z_func,
                                         x_range=x_range,
                                         y_range = y_range,
