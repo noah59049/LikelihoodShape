2ef72a14033db8a42897c0b15b0ef53ce9e24058
noah <ntholbro@uvm.edu>
2026-05-08 17:13:06 -0700

    the hat matrix logo is here

---
+++ hat_matrix_logo.py
@@ -0,0 +1,21 @@
+from manim import *
+
+class HatMatrixScene(Scene):
+    def construct(self):
+        brackets = MathTex(
+            r"\begin{bmatrix}"
+            r"\quad & \quad \\"
+            r"\quad & \quad"
+            r"\end{bmatrix}"
+        )
+        self.add(brackets)
+
+        triangle = Triangle(color = PURPLE, 
+                            fill_color = PURPLE, 
+                            fill_opacity = 1,
+                            )
+        triangle.next_to(brackets, UP)
+        triangle.stretch_to_fit_height(2)
+        triangle.scale(0.9)
+        triangle.shift(UP * 0.4)
+        self.add(triangle)
