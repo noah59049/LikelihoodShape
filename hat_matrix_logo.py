9edfed0f152d5241643ad86a5561b65d9a5b3daa
noah <ntholbro@uvm.edu>
2026-05-10 23:17:34 -0700

    buckle has rounded corners

--- hat_matrix_logo.py
+++ hat_matrix_logo.py
@@ -38,7 +38,7 @@ class HatMatrixScene(Scene):
         self.add(buckle_strap)
 
         # Buckle
-        buckle = Rectangle(color = WHITE, width = triangle.width / 3.45, height = buckle_rect.height)
+        buckle = RoundedRectangle(color = WHITE, width = triangle.width / 3.45, height = buckle_rect.height, corner_radius = buckle_rect.height / 10)
         buckle.move_to(buckle_strap)
         self.add(buckle)
 
