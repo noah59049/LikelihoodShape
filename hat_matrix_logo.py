fea5f142b19585484595703ba3fdac3ba752bf73
noah <ntholbro@uvm.edu>
2026-05-10 20:04:19 -0700

    the hat matrix has a right arm

--- hat_matrix_logo.py
+++ hat_matrix_logo.py
@@ -20,3 +20,10 @@ class HatMatrixScene(Scene):
         triangle.scale(0.9)
         triangle.shift(UP * 0.4)
         self.add(triangle)
+
+        print(brackets.get_right())
+        print(brackets.get_center())
+
+        arc1 = Arc(start_angle = 3 * PI / 2, angle = PI / 2, radius = 1).move_to(brackets.get_right())
+        arc1.shift(arc1.width / 2 * RIGHT + arc1.height / 2 * UP)
+        self.add(arc1)
