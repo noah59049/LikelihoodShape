ebf1a10cb9108f20db270ca30edd5ce9944bc7bb
noah <ntholbro@uvm.edu>
2026-05-10 22:54:33 -0700

    add hands

--- hat_matrix_logo.py
+++ hat_matrix_logo.py
@@ -1,3 +1,4 @@
+import math
 from manim import *
 
 class HatMatrixScene(Scene):
@@ -24,12 +25,25 @@ class HatMatrixScene(Scene):
         print(brackets.get_right())
         print(brackets.get_center())
 
+        def get_hand(start_point, length = 0.13, theta = math.pi / 6):
+            line1 = Line(start = start_point, end = start_point + math.cos(theta) * length * UP + math.sin(theta) * length * RIGHT)
+            line2 = Line(start = start_point, end = start_point + length * UP)
+            line3 = Line(start = start_point, end = start_point + math.cos(theta) * length * UP + math.sin(theta) * length * LEFT)
+            return VGroup(line1, line2, line3)
+
         # Right arm
         arc1 = Arc(start_angle = 3 * PI / 2, angle = PI / 2, radius = 0.3).move_to(brackets.get_right())
         arc1.shift(arc1.width / 2 * RIGHT + arc1.height / 2 * UP)
         self.add(arc1)
 
+        hand1 = get_hand(start_point = arc1.get_corner(UR))
+        self.add(hand1)
+
         # Left arm
         arc2 = Arc(start_angle = PI, angle = PI / 2, radius = 0.3).move_to(brackets.get_left())
         arc2.shift(arc2.width / 2 * LEFT + arc2.height / 2 * UP)
         self.add(arc2)
+
+        hand2 = get_hand(start_point = arc2.get_corner(UL))
+        self.add(hand2)
+        
