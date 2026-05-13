f56ca887071acfcd793634908e92c81e219d1eac
noah <ntholbro@uvm.edu>
2026-05-13 13:46:45 -0700

    put the stuff in another voiceover chunk

--- linear_to_logistic.py
+++ linear_to_logistic.py
@@ -13,7 +13,7 @@ Y = np.array(df["target"])
 
 class LinearLogisticScene(VoiceoverScene):
     def construct(self):
-        self.set_speech_service(StitcherService(r"/Users/noah/Convex/LikelihoodShape/podcasts/linear_to_logistic_podcast_994.wav",
+        self.set_speech_service(StitcherService(r"/Users/noah/Convex/LikelihoodShape/podcasts/linear_to_logistic_podcast_995.wav",
                 cache_dir="/Users/noah/Convex/LikelihoodShape/cache_dir",
                 min_silence_len=2000,
                 keep_silence=(0,0)))
@@ -213,7 +213,7 @@ class LinearLogisticScene(VoiceoverScene):
                                             ([3],[5], {"path_arc": -PI}),
                                             ([5],[3], {"path_arc":-PI})))
 
-        with self.voiceover("Take the reciprocal again, and If you’re familiar with machine learning, you may recognize a sigmoid here.") as tracker:
+        with self.voiceover("Take the reciprocal again, and if you’re familiar with machine learning, you may recognize a sigmoid here.") as tracker:
             self.play(TransformByGlyphMap(sigmoid5, sigmoid6,
                                             ([0,1], [2,3])))
             
@@ -241,37 +241,38 @@ class LinearLogisticScene(VoiceoverScene):
             self.play(DrawBorderThenFill(axes), Write(axis_labels), run_time = 0.5)
             self.play(Create(func))
 
-        z_tracker = ValueTracker(-5)
+        with self.voiceover("To see why, if you plug in a small value of z, the denominator becomes very large, and your function approaches 0 very fast. If z is large, then the exponential term becomes near zero, and the fraction evaluates to almost 1.") as tracker:
+            z_tracker = ValueTracker(-5)
 
-        sliding_dot = always_redraw(lambda: Dot(
-            axes.c2p(z_tracker.get_value(), sigmoid(z_tracker.get_value())),
-            color=YELLOW,
-            radius=0.1,
-        ))
-        v_line = always_redraw(lambda: DashedLine(
-            axes.c2p(z_tracker.get_value(), 0),
-            axes.c2p(z_tracker.get_value(), sigmoid(z_tracker.get_value())),
-            color=YELLOW,
-            stroke_width=2,
-        ))
-        h_line = always_redraw(lambda: DashedLine(
-            axes.c2p(0, sigmoid(z_tracker.get_value())),
-            axes.c2p(z_tracker.get_value(), sigmoid(z_tracker.get_value())),
-            color=YELLOW,
-            stroke_width=2,
-        ))
-        formula = always_redraw(lambda: MathTex(
-            r"\sigma(z)=\frac{1}{1+e^{" + f"{-z_tracker.get_value():.1f}" + r"}}",
-        ).to_corner(UL))
-        sigma_label = VGroup(
-            MathTex(r"\sigma(z) ="),
-            DecimalNumber(sigmoid(z_tracker.get_value()), num_decimal_places=3, color=YELLOW),
-        ).arrange(RIGHT, buff=0.15).next_to(formula, DOWN)
-        sigma_label[1].add_updater(lambda m: m.set_value(sigmoid(z_tracker.get_value())))
+            sliding_dot = always_redraw(lambda: Dot(
+                axes.c2p(z_tracker.get_value(), sigmoid(z_tracker.get_value())),
+                color=YELLOW,
+                radius=0.1,
+            ))
+            v_line = always_redraw(lambda: DashedLine(
+                axes.c2p(z_tracker.get_value(), 0),
+                axes.c2p(z_tracker.get_value(), sigmoid(z_tracker.get_value())),
+                color=YELLOW,
+                stroke_width=2,
+            ))
+            h_line = always_redraw(lambda: DashedLine(
+                axes.c2p(0, sigmoid(z_tracker.get_value())),
+                axes.c2p(z_tracker.get_value(), sigmoid(z_tracker.get_value())),
+                color=YELLOW,
+                stroke_width=2,
+            ))
+            formula = always_redraw(lambda: MathTex(
+                r"\sigma(z)=\frac{1}{1+e^{" + f"{-z_tracker.get_value():.1f}" + r"}}",
+            ).to_corner(UL))
+            sigma_label = VGroup(
+                MathTex(r"\sigma(z) ="),
+                DecimalNumber(sigmoid(z_tracker.get_value()), num_decimal_places=3, color=YELLOW),
+            ).arrange(RIGHT, buff=0.15).next_to(formula, DOWN)
+            sigma_label[1].add_updater(lambda m: m.set_value(sigmoid(z_tracker.get_value())))
 
-        self.play(FadeIn(sliding_dot), FadeIn(v_line), FadeIn(h_line), FadeIn(sigma_label), TransformMatchingTex(sigmoid_defn, formula))
-        self.play(z_tracker.animate.set_value(5), run_time=5, rate_func=linear)
-        self.play(FadeOut(sliding_dot), FadeOut(v_line), FadeOut(h_line), FadeOut(sigma_label), TransformMatchingTex(formula, sigmoid_defn))
+            self.play(FadeIn(sliding_dot), FadeIn(v_line), FadeIn(h_line), FadeIn(sigma_label), TransformMatchingTex(sigmoid_defn, formula))
+            self.play(z_tracker.animate.set_value(5), run_time=5, rate_func=linear)
+            self.play(FadeOut(sliding_dot), FadeOut(v_line), FadeOut(h_line), FadeOut(sigma_label), TransformMatchingTex(formula, sigmoid_defn))
 
         with self.voiceover("If you multiply the top and bottom of the fraction by e to the z, ") as tracker:
             self.play(TransformByGlyphMap(sigmoid_defn, sigmoid_defn2,
