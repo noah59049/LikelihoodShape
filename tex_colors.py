0473ca6a412e6c7d08fe425b90a7c4fe8c7a8853
noah <ntholbro@uvm.edu>
2026-05-15 13:32:16 -0700

    create file for colored math tex

---
+++ tex_colors.py
@@ -0,0 +1,28 @@
+from manim import *
+
+X_color = RED
+beta_color = BLUE
+Y_color = GREEN
+z_color = YELLOW
+p_color = GREEN
+L_color = PINK
+
+
+def color_math(tex: MathTex) -> MathTex:
+    tex.set_color_by_tex(r"\beta", beta_color, substring=False)
+    tex.set_color_by_tex("X", X_color, substring=False)
+    tex.set_color_by_tex("Y", Y_color, substring=False)
+    tex.set_color_by_tex("y", Y_color, substring=False)
+    tex.set_color_by_tex("z", z_color, substring=False)
+    tex.set_color_by_tex("p", p_color, substring=False)
+    tex.set_color_by_tex("L", L_color, substring=False)
+    tex.set_color_by_tex("l", L_color, substring=False)
+    tex.set_color_by_tex(r"\ell", L_color, substring=False)
+    return tex
+
+
+class ColoredMathTex(MathTex):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault('substrings_to_isolate', [r"\beta", r"\ell", "X", "Y", "L", "y", "z"])
+        super().__init__(*args, **kwargs)
+        color_math(self)
