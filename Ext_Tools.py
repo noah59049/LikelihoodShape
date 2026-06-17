9ffdac10be7a4417695954a0458fad6e452d58ae
noah <ntholbro@uvm.edu>
2026-06-16 19:53:15 -0700

    add a few more utility functions

--- Ext_Tools.py
+++ Ext_Tools.py
@@ -5,6 +5,9 @@ from manim import *
 
 # Written by GniLudio
 class Combine(AnimationGroup):
+    """
+    If you want to combine animations on the same object(s):
+    """
     def _setup_scene(self, scene: Scene) -> None:
         super()._setup_scene(scene)
         self._scene = scene
@@ -19,3 +22,23 @@ class Combine(AnimationGroup):
             animation._setup_scene(self._scene)
             animation.begin()
             animation.interpolate(alpha)
+
+# Written by nikolaj
+def count_animations(scene : Scene, **config_kwargs):
+    """
+    Here's a stupid little utility function. I've got a scene which builds its list of animation depending on some starting input (for example, a BFS animation whose length depends on the actual graph input). The total number of animations can get quite large, and I needed to be able to count how many animations were being played for a given input without having to wait for the actual animation to render and play.
+    """
+    options = {
+        "frame_size": (1,1),
+        "frame_rate": 1,
+        "save_last_frame": True,
+        "dry_run": True,
+        "verbosity": "ERROR",
+        "progress_bar": "none",
+    } | config_kwargs
+    with tempconfig(options):
+        if not isinstance(scene, Scene):
+            scene = scene()
+        scene.render()
+        return scene.renderer.num_plays
+    
